a4a983e4ef8aeead5429e81ffa8a03f6fe93d79e
Manuel Castro <maacaro@gmail.com>
2024-11-06 17:15:28 -0400

    test

--- orc/__init__.py
+++ orc/__init__.py
@@ -4,29 +4,35 @@ import json
 import os
 from . import new_orchestrator
 
-LOGLEVEL = os.environ.get('LOGLEVEL', 'DEBUG').upper()
-logging.basicConfig(level=LOGLEVEL)
+# LOGLEVEL = os.environ.get('LOGLEVEL', 'DEBUG').upper()
+# logging.basicConfig(level=LOGLEVEL)
+
 
 async def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Python HTTP trigger function processed a request.")
 
     req_body = req.get_json()
-    conversation_id = req_body.get('conversation_id')
-    question = req_body.get('question')
-    client_principal_id = req_body.get('client_principal_id')
-    client_principal_name = req_body.get('client_principal_name') 
-    url = req_body.get('url', '')
-    if not client_principal_id or client_principal_id == '':
-        client_principal_id = '00000000-0000-0000-0000-000000000000'
-        client_principal_name = 'anonymous'    
-    client_principal = {
-        'id': client_principal_id,
-        'name': client_principal_name
-    }
+    conversation_id = req_body.get("conversation_id")
+    question = req_body.get("question")
+    client_principal_id = req_body.get("client_principal_id")
+    client_principal_name = req_body.get("client_principal_name")
+    url = req_body.get("url", "")
+    if not client_principal_id or client_principal_id == "":
+        client_principal_id = "00000000-0000-0000-0000-000000000000"
+        client_principal_name = "anonymous"
+    client_principal = {"id": client_principal_id, "name": client_principal_name}
 
     if question:
-        result = await new_orchestrator.run(conversation_id, question, url, client_principal)
+        result = await new_orchestrator.run(
+            conversation_id, question, url, client_principal
+        )
 
-        return func.HttpResponse(json.dumps(result), mimetype="application/json", status_code=200)
+        return func.HttpResponse(
+            json.dumps(result), mimetype="application/json", status_code=200
+        )
     else:
-        return func.HttpResponse('{"error": "no question found in json input"}', mimetype="application/json", status_code=200)
+        return func.HttpResponse(
+            '{"error": "no question found in json input"}',
+            mimetype="application/json",
+            status_code=200,
+        )
